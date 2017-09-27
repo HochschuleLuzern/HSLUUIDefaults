5465fab720696eed89ce5186bbeedd9df4513192
Raphael Heer <raphael.heer@hslu.ch>
2017-09-27 12:35:33 +0200

    Hide DTPLs in special folders
    This patches hides and autoselects didactic templates in special cases:
    - Select "closed"-groups as default
    - Only "standart"-folders in fileexchange-folders (hide other templates)
    - Only "postbox"-folders in postbox-folders (hide other templates)
    - Only "standart"-groups in groupfolders (hide other templates)

--- classes/class.ilHSLUUIDefaultsUIHookGUI.php
+++ classes/class.ilHSLUUIDefaultsUIHookGUI.php
@@ -12,27 +12,92 @@ include_once("./Services/UIComponent/classes/class.ilUIHookPluginGUI.php");
 * @ingroup ServicesUIComponent
 */
 class ilHSLUUIDefaultsUIHookGUI extends ilUIHookPluginGUI {
-	function getHTML($a_comp, $a_part, $a_par = array())
+    
+    function getHTML($a_comp, $a_part, $a_par = array())
 	{
-		
+	    global $tpl;
 		if ($a_comp == "Services/MainMenu" && $a_part == "main_menu_list_entries")
 		{
 			global $ilias;
 			
-			//gruppe erstellen default
-			if(isset($_GET['new_type']) && $_GET['new_type']=='grp' && isset($_GET['cmd']) && $_GET['cmd']=='create'   ){
-				return array("mode" => ilUIHookPluginGUI::APPEND, "html" =>
-'
-<script type="text/javascript">
-il.Util.addOnLoad(function() {
-	jQuery("#didactic_type_dtpl_1").click();
-});
-</script>
-');
+			// On create group or folder with didactic templates
+			if($this->specialMethodsExists() && isset($_GET['new_type']) && ($_GET['new_type']=='fold' || $_GET['new_type']=='grp') && isset($_GET['cmd']) && $_GET['cmd']=='create')
+			{
+			    $script = '';
+			    
+			    // On create folder
+			    if($_GET['new_type']=='fold')
+			    {
+			        // In a fileexchange-folder -> only normal folders allowed (no didactic template)
+			        if(ilDidacticTemplateObjSettings::isSpecialFolder($_GET['ref_id'], 'Dateiaustausch') ||
+                       ilDidacticTemplateObjSettings::isInSpecialFolder($_GET['ref_id'], 'Dateiaustausch'))
+			        {
+			            $script = $this->hideDTPLsOnCreate(0);
+			        }
+			        // In a postbox-folder -> only postboxes allowed
+			        else if(ilDidacticTemplateObjSettings::isSpecialFolder($_GET['ref_id'], 'Briefkasten'))
+			        {
+			            $script = $this->hideDTPLsOnCreate(ilDidacticTemplateObjSettings::lookupTemplateIdByName('Briefkasten'));
+			        }
+			    }
+			    // On create group
+			    else //if($_GET['new_type']=='grp')
+			    {
+			        // In a groupfolder -> only open groups allowed
+			        if(ilDidacticTemplateObjSettings::isSpecialFolder($_GET['ref_id'], 'Gruppenordner'))
+			        {
+			            $script = $this->hideDTPLsOnCreate(0);
+			        }
+			        // Somewhere else -> Default is closed group
+			        else
+			        {
+			            $script = $this->selectDTPLOnCreate(1);
+			        }
+			    }
+			    $tpl->addOnLoadCode($script);
 			}
 		}
 
 		return array("mode" => ilUIHookPluginGUI::KEEP, "html" => "");
+	}
+	
+	/**
+	 * Creates jquery-code to hide other didactic templates
+	 * @param integer $allowed_dtpl_id ID of the only allowed didactic template
+	 * @return string
+	 */
+	private function hideDTPLsOnCreate($allowed_dtpl_id)
+	{
+	    return '$("#didactic_type").children().each(function(index){
+                  if(!$.contains(this, $("#didactic_type_dtpl_'.$allowed_dtpl_id.'")[0])){
+                    $(this).hide();
+                  }else{
+                    $(this).find("input").attr("checked", true);
+                  }
+                });';
+	}
+	
+	/**
+	 * Creates jquery-code to select a default didactic template
+	 * @param integer $select_dtpl_id ID of didactic template to select
+	 * @return string
+	 */
+	private function selectDTPLOnCreate($select_dtpl_id)
+	{
+	    return '$("#didactic_type_dtpl_'.$select_dtpl_id.'").prop("checked", true);';
+	}
+	
+	/**
+	 * Checks if the needed methods from the hslu-patches exists
+	 * @return boolean
+	 */
+	private function specialMethodsExists()
+	{
+	    $exist = method_exists('ilDidacticTemplateObjSettings', 'isSpecialFolder');
+	    $exist &= method_exists('ilDidacticTemplateObjSettings', 'isInSpecialFolder');
+	    $exist &= method_exists('ilDidacticTemplateObjSettings', 'lookupTemplateIdByName');
+	    
+	    return $exist;
 	}
 }
 ?>
